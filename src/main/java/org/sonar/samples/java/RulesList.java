c6b8b9a45d1c46cfc5e4a0c1ff03e140670e8524
MohmedAbdAllah <eng.mohmed.abdallah@outlook.com>
2024-09-30 12:25:45 +0300

    Add sonarqube custom rule - java

---
+++ src/main/java/org/sonar/samples/java/RulesList.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2012-2024 SonarSource SA - mailto:info AT sonarsource DOT com
+ * This code is released under [MIT No Attribution](https://opensource.org/licenses/MIT-0) license.
+ */
+package org.sonar.samples.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.sonar.plugins.java.api.JavaCheck;
+import org.sonar.samples.java.checks.MyFirstCustomCheck;
+
+public final class RulesList {
+
+  private RulesList() {
+  }
+
+  public static List<Class<? extends JavaCheck>> getChecks() {
+    List<Class<? extends JavaCheck>> checks = new ArrayList<>();
+    checks.addAll(getJavaChecks());
+    return Collections.unmodifiableList(checks);
+  }
+
+  /**
+   * These rules are going to target MAIN code only
+   */
+  public static List<Class<? extends JavaCheck>> getJavaChecks() {
+    return Collections.unmodifiableList(Arrays.asList(
+      MyFirstCustomCheck.class
+      ));
+  }
+
+  /**
+   * These rules are going to target TEST code only
+   */
+  public static List<Class<? extends JavaCheck>> getJavaTestChecks() {
+    return Collections.unmodifiableList(Arrays.asList(
+      MyFirstCustomCheck.class));
+  }
+}
---
+++ src/test/java/org/sonar/samples/java/MyJavaFileCheckRegistrarTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012-2024 SonarSource SA - mailto:info AT sonarsource DOT com
+ * This code is released under [MIT No Attribution](https://opensource.org/licenses/MIT-0) license.
+ */
+package org.sonar.samples.java;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.java.checks.verifier.TestCheckRegistrarContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MyJavaFileCheckRegistrarTest {
+
+  @Test
+  void checkRegisteredRulesKeysAndClasses() {
+    TestCheckRegistrarContext context = new TestCheckRegistrarContext();
+
+    MyJavaFileCheckRegistrar registrar = new MyJavaFileCheckRegistrar();
+    registrar.register(context);
+
+    assertThat(context.mainRuleKeys).extracting(RuleKey::toString).containsExactly(
+      "Qestit-java:MyFirstCustomCheck");
+
+    assertThat(context.mainCheckClasses).extracting(Class::getSimpleName).containsExactly(
+      "MyFirstCustomCheck");
+
+  }
+
+}
